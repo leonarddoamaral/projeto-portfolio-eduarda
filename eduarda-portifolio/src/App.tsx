796af5b94980ef74f40cf724bdca145927a2f4e3
Leonardo do Amaral Quinquio <leonarddoamaral@gmail.com>
2026-01-18 12:04:56 -0300

    Começando home
    criado o titulo, subtitulo, e nome. Baseado totalmente no portfolio criado pela cliente. Criação de variaveis de cores de font e background.

---
+++ eduarda-portifolio/src/App.tsx
@@ -0,0 +1,24 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <main>
+      <div className='container-nome'>
+        <p>Eduarda Martinez Nogueira</p>
+      </div>
+      
+      <div className='container-titulo'>
+        <h1 className='titulo'>Portfólio</h1>
+        <h3 className='subTitulo'>Arquitetura e Urbanismo</h3>
+      </div>
+    </main>
+      
+    </>
+  )
+}
+
+export default App
